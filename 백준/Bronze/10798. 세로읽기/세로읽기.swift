a13a3b85b50a3017c90b0bcd7af95b6d85ba5af4
CHAEHYUN PARK <clairebluepark@gmail.com>
2025-07-14 12:48:07 +0900

    [Bronze I] Title: 세로읽기, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+var board: [[String]] = .init(repeating: .init(repeating: "", count: 15),count: 5)
+
+for i in 0..<5 {
+    let input = readLine()!.map{ String($0) }
+
+    for j in 0..<input.count {
+        board[i][j] = input[j]
+    }
+}
+
+var answer = ""
+
+for i in 0..<15 {
+    for j in 0..<5 {
+        if board[j][i] != "" {
+            answer += board[j][i]
+        }
+    }
+}
+
+print(answer)
